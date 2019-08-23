de2f7e391cfae82d379db2032a61583d5d8ee23a
bruno <brunosouza97ti@gmail.com>
2019-08-22 22:24:26 -0300

    Add new test

--- src/test/java/ProjectConfig.java
+++ src/test/java/ProjectConfig.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -22,8 +23,8 @@ public class ProjectConfig {
         driver.get(url);
     }
 
-//    @After
-//    public void tearDown() {
-//        driver.quit();
-//    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
--- src/test/java/SimulationTest.java
+++ src/test/java/SimulationTest.java
@@ -9,34 +9,56 @@ import static junit.framework.TestCase.assertTrue;
 public class SimulationTest extends ProjectConfig {
 
     @Test
-    public void simpleSimulation() {
+    public void testCorrectSimulation() {
         WebDriverWait wait = new WebDriverWait(driver, 5);
 
         driver.findElement(By.id("valorAplicar")).sendKeys("200,00");
         driver.findElement(By.id("valorInvestir")).sendKeys("50,00");
         driver.findElement(By.id("tempo")).sendKeys("5");
         driver.findElement(By.cssSelector(".btSelect > .btn")).click();
-        driver.findElement(By.linkText("Anos")).click();
+        driver.findElement(By.linkText("Meses")).click();
         driver.findElement(By.cssSelector(".btnSimular")).click();
 
         WebElement finalResult = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/div[2]/span[2]"));
         wait.until(ExpectedConditions.visibilityOfAllElements(finalResult));
-        assertTrue(finalResult.getText().contains("R$ 3.604"));
+        assertTrue(finalResult.getText().contains("R$ 456"));
     }
 
     @Test
-    public void errorSimulation() {
+    public void testValueErrorSimulation() {
         WebDriverWait wait = new WebDriverWait(driver, 5);
 
         driver.findElement(By.id("valorAplicar")).sendKeys("10,00");
         driver.findElement(By.id("valorInvestir")).sendKeys("50,00");
         driver.findElement(By.id("tempo")).sendKeys("5");
         driver.findElement(By.cssSelector(".btSelect > .btn")).click();
-        driver.findElement(By.linkText("Anos")).click();
+        driver.findElement(By.linkText("Meses")).click();
         driver.findElement(By.cssSelector(".btnSimular")).click();
 
-        WebElement finalResult = driver.findElement(By.id("valorAplicar-error"));
-        wait.until(ExpectedConditions.visibilityOfAllElements(finalResult));
-        assertTrue(finalResult.getText().contains("Valor mínimo de 20.00"));
+        WebElement valueError = driver.findElement(By.id("valorAplicar-error"));
+        wait.until(ExpectedConditions.visibilityOfAllElements(valueError));
+        assertTrue(valueError.getText().contains("Valor mínimo de 20.00"));
+    }
+
+    @Test
+    public void testAllErrors() {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        driver.findElement(By.id("valorAplicar")).sendKeys("10,00");
+        driver.findElement(By.id("valorInvestir")).sendKeys("50,00");
+        driver.findElement(By.id("tempo")).sendKeys("5");
+        driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[1]/a")).click();
+
+        WebElement valueError = driver.findElement(By.id("valorAplicar-error"));
+        wait.until(ExpectedConditions.visibilityOfAllElements(valueError));
+        assertTrue(valueError.getText().contains("Valor mínimo de 20.00"));
+
+        WebElement valueInvestError = driver.findElement(By.id("valorInvestir-error"));
+        wait.until(ExpectedConditions.visibilityOfAllElements(valueInvestError));
+        assertTrue(valueInvestError.getText().contains("Valor mínimo de 20.00"));
+
+        WebElement requiredField = driver.findElement(By.id("tempo-error"));
+        wait.until(ExpectedConditions.visibilityOfAllElements(requiredField));
+        assertTrue(requiredField.getText().contains("Obrigatório"));
     }
 }
